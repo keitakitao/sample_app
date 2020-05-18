96b6a05fdbac132d5198ca705be1267a4a2e1dab
keitakitao <kitaokeita.kk@gmail.com>
2020-05-18 18:50:26 +0900

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -9,6 +11,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+
   end
 
 end
